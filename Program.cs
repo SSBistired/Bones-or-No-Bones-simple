46631a31472f544746c1ceab8a346f8a3691e8a7
User <User@LAPTOP-5G8N9L5T>
2021-11-09 15:59:11 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonesOrNoBones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// instanciates new object for random
+            Random rnd = new Random();
+
+            ///sets array 
+            string[] Day = { "It's a Bones Day", "It's a No Bones Day" };
+
+            //random selects from Day string array
+            int index = rnd.Next(Day.Length);
+            Console.WriteLine(Day[index]);
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
